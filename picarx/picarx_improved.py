55c0163c568beefdea2739a80cf138f8cb664c7a
Liam Bouffard <liamtbo@gmail.com>
2026-01-22 12:14:25 -0800

    working on movement for week 2

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -268,34 +268,35 @@ class Picarx(object):
         self.ultrasonic.close()
 
 def main():
-    px = Picarx()
-    while(1):
-        command = int(input(f'commands:\n\tforward: 1\n\tbackward: 2\n\tparallel-park left:3 \n\tparallel-park right: 4\n\tthree-point left: 5\n\tthree point right: 6\n'))
+    try:
+        px = Picarx()
+        while(1):
+            command = int(input(f'commands:\n\tforward: 1\n\tbackward: 2\n\tparallel-park left:3 \n\tparallel-park right: 4\n\tthree-point left: 5\n\tthree point right: 6\n'))
 
-        if command == 1:
-            angle_deg = float(input('angle: '))
-            px.set_dir_servo_angle(angle_deg)
-            px.forward(50)
-        elif command == 2:
-            angle_deg = float(input('angle: '))
-            px.set_dir_servo_angle(angle_deg)
-            px.backward(50)
-        elif command == 3:
-            px.set_dir_servo_angle(30)
-            px.backward(50)
-            px.set_dir_servo_angle(-30)
-            px.forward(50)
-        elif command == 4:
-            px.set_dir_servo_angle(-30)
-            px.backward(50)
-            px.set_dir_servo_angle(30)
-            px.forward(30)
-
-
-
-
-    time.sleep(1)
-    px.stop()
+            if command == 1:
+                angle_deg = float(input('angle: '))
+                px.set_dir_servo_angle(angle_deg)
+                px.forward(50)
+            elif command == 2:
+                angle_deg = float(input('angle: '))
+                px.set_dir_servo_angle(angle_deg)
+                px.backward(50)
+            elif command == 3:
+                px.set_dir_servo_angle(30)
+                px.backward(50)
+                px.set_dir_servo_angle(-30)
+                px.forward(50)
+            elif command == 4:
+                px.set_dir_servo_angle(-30)
+                px.backward(50)
+                px.set_dir_servo_angle(30)
+                px.forward(30)
+            else:
+                time.sleep(1)
+                px.stop()
+    except KeyboardInterrupt:
+        print(f'exiting')
+        px.stop()
 
 
 if __name__ == "__main__":
